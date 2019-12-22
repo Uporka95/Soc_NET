66ee36f05a7c02e53186b704edbfbb75b67fd19f
Uporka95 <maxitronix197@gmail.com>
2019-12-22 15:32:34 +0500

    23w

---
+++ Soc_Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Soc_Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Socket Soc_Connetion = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			IPEndPoint IPServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+
+			Soc_Connetion.Connect(IPServer);
+
+			byte[] buf = new byte[255];
+			Soc_Connetion.Send(Encoding.UTF8.GetBytes("Привет сервер"));
+			Soc_Connetion.Receive(buf);
+			Console.WriteLine(Encoding.UTF8.GetString(buf));
+
+			Soc_Connetion.Close();
+			Console.ReadLine();
+		}
+	}
+}
